a76db0737d67e440056ae13f7b6b48983aca0aee
Ruth <osukukuruth@gmail.com>
2023-07-08 07:38:12 -0400

    Done Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
 end
+
+"Hello.".sentence? # => true
+"Is it raining?".question? # => true
+"Wow!".exclamation? # => true
+"This is a sentence. This is another sentence!".count_sentences # => 2
